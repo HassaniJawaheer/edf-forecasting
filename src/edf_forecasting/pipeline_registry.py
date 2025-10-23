4d23ff8812c63ac23f3c9758cdb6f8d77d6d9c73
Jawaheer Hassani <jawaheer.hassani@synchrotron-soleil.fr>
2025-10-23 21:30:15 +0200

    Mise à jour

--- src/edf_forecasting/pipeline_registry.py
+++ src/edf_forecasting/pipeline_registry.py
@@ -14,13 +14,13 @@ def register_pipelines() -> dict[str, Pipeline]:
     pipelines = find_pipelines()
     
     #pipelines["__default__"] = sum(pipelines.values())
-    pipelines["full"] = (
+    pipelines["xgboost_time_series"] = (
         pipelines["fetch_raw_data"]
         + pipelines["process_data"]
         + pipelines["tune_xgboost_time_series"]
         + pipelines["train_xgboost_time_series"]
     )
 
-    pipelines["__default__"] = pipelines["full"]
+    pipelines["__default__"] = pipelines["xgboost_time_series"]
 
     return pipelines
