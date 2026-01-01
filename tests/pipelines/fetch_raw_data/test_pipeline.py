7733116225c648c4606c1ce3cec48ed6a4df641d
Jawaheer Hassani <jawaheer.hassani@synchrotron-soleil.fr>
2026-01-01 10:41:09 +0100

    Add fetch data test

--- tests/pipelines/fetch_raw_data/test_pipeline.py
+++ tests/pipelines/fetch_raw_data/test_pipeline.py
@@ -7,3 +7,11 @@ Kedro recommends using `pytest` framework, more info about it can be found
 in the official documentation:
 https://docs.pytest.org/en/latest/getting-started.html
 """
+
+from edf_forecasting.pipelines.fetch_raw_data.pipeline import create_pipeline
+
+def test_fetch_raw_data_pipeline_creation():
+    pipeline = create_pipeline()
+
+    assert pipeline is not None
+    assert len(pipeline.nodes) > 0
