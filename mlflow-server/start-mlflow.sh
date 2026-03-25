a0e3503fb1305444ae10c539cbb062d2bc542061
Jawaheer Hassani <jawaheer.hassani@synchrotron-soleil.fr>
2026-03-25 22:09:13 +0100

    mise à jour des images : minio, mlflow, api et monitoring

--- mlflow-server/start-mlflow.sh
+++ mlflow-server/start-mlflow.sh
@@ -1,9 +1,7 @@
 docker run -d \
   --name mlflow-server \
-  --network edf-network \
+  --network edf-forecasting \
   -p 5000:5000 \
-  -e MLFLOW_S3_ENDPOINT_URL=http://minio:9000 \
-  -e AWS_ACCESS_KEY_ID=admin \
-  -e AWS_SECRET_ACCESS_KEY=password \
-  -e AWS_DEFAULT_REGION=us-east-1 \
+  --env-file .env \
+  -v mlflow-data:/mlflow/data \
   edf-mlflow
