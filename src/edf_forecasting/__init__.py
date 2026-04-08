e626075951544ac9dce41c3e4d4f4610eef7262c
Jawaheer Hassani <jawaheer.hassani@synchrotron-soleil.fr>
2026-04-08 22:00:09 +0200

    update

--- src/edf_forecasting/__init__.py
+++ src/edf_forecasting/__init__.py
@@ -6,4 +6,5 @@ __version__ = "0.1"
 import mlflow
 import os
 
-mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI", "file:/mlruns"))
+tracking_uri = os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5000")
+mlflow.set_tracking_uri(tracking_uri)
